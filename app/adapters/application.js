eaa0943cc90a28ae5fd776f16783eae4c2c99962
bhaskarmelkani <bhaskar.melkani@oyorooms.com>
2016-01-11 23:50:32 +0530

    update

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,10 +2,6 @@ import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
   host: 'https://api.github.com',
-  headers: {
-    Authorization: "token 6258d72304b8c8e68fc286666109d5e41c6c8978" // replace!
-  },
-
   updateRecord(store, type, snapshot) {
     const url = `${this.host}/user/starred/${snapshot.attr('fullName')}`;
     return this.ajax(url, "PUT", {});
--- app/serializers/user.js
+++ app/serializers/user.js
@@ -1,26 +1,25 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPISerializer.extend({
-  normalizeFindRecordResponse(store, type, payload){
-    console.log(store);
-    console.log(type);
-    console.log(payload);
-    return{
+  
+  normalizeFindRecordResponse(store, type, payload) {
+    return {
       data: {
         id: payload.login,
         type: type.modelName,
-        attributes:{
+        attributes: {
           name: payload.name,
           publicRepos: payload.public_repos
-        }
-      },
-      relationships: {
-        repos:{
-          links: {
-            related: payload.repos_url
+        },
+        relationships: {
+          repos: {
+            links: {
+              related: payload.repos_url
+            }
           }
         }
       }
     }
   }
+  
 });
